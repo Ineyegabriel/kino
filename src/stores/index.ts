31901b5eaddd1d34bb2f0fd988dbe6a5addbccc7
Ineyegabriel <GIneye@globalization-partners.com>
2023-10-19 14:50:09 +0100

    removing redundant store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,9 @@
 import MoviesStore from './moviesStore';
-import WatchListStore from './watchListStore';
 
 export default class RootStore {
   public moviesStore: MoviesStore;
-  public watchListStore: WatchListStore;
 
   constructor() {
     this.moviesStore = new MoviesStore();
-    this.watchListStore = new WatchListStore();
   }
 }
